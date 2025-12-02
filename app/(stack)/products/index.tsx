2642ad765b5071d4487e661d22b7ed2a0940956d
Isaac Bustamante <izaack.97@gmail.com>
2025-12-01 17:38:26 -0700

    Added raw data

--- app/(stack)/products/index.tsx
+++ app/(stack)/products/index.tsx
@@ -1,10 +1,15 @@
+import { products } from '@/store/products.store'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 
 const ProductsScreen = () => {
   return (
-    <View>
-      <Text>ProductsScreen</Text>
+    <View className='flex flex-1'>
+      <FlatList 
+        data={ products }
+        keyExtractor={( item ) => item.id}
+        renderItem={(item) => <Text className='text-primary text-2xl font-work-medium mt-10 p-4'>{ JSON.stringify( item, null, 2 ) }</Text>}
+      />
     </View>
   )
 }
